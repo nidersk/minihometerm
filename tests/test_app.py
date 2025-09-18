45d2f36f4415f170c76d5a4a25264c604edaf219
Peter Nitschneider <peter@nitschneider.sk>
2025-09-18 16:23:54 +0200

    test(app): fix headless mode setup in test_build_returns_widget

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,15 @@
-from kivy.uix.screenmanager import ScreenManager
-
-from minihometerm.app import MiniHomeTerm
-
-
 def test_app_title():
+    from minihometerm.app import MiniHomeTerm
+
     app = MiniHomeTerm()
     assert app.title == "My Kivy App"
 
 
 def test_build_returns_widget():
+    from kivy.uix.screenmanager import ScreenManager
+
+    from minihometerm.app import MiniHomeTerm
+
     app = MiniHomeTerm()
     root = app.build()
     # Root should be a Kivy widget (BoxLayout because KV defines one)
@@ -17,6 +18,8 @@ def test_build_returns_widget():
 
 
 def test_on_click_me_prints(capsys):
+    from minihometerm.app import MiniHomeTerm
+
     app = MiniHomeTerm()
     app.on_click_me()
     captured = capsys.readouterr()
